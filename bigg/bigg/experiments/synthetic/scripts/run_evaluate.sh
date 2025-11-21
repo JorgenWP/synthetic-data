6ee8c4154e0a8d05473d7139614000ab19f31725
JorgenWP <jorgen_wp@hotmail.com>
2025-11-21 16:58:38 +0100

    Updated evaluation script

--- bigg/bigg/experiments/synthetic/scripts/run_evaluate.sh
+++ bigg/bigg/experiments/synthetic/scripts/run_evaluate.sh
@@ -5,7 +5,7 @@ prediction_file=../../../../results/lobster/DFS-blksize--1-b-80/epoch-25.ckpt.gr
 test_file=../../../../data/lobster-DFS/test-graphs.pkl
 metrics=degree,clustering,spectral
 
-python evaluate.py \
+python ../evaluate.py \
   -pred_file $prediction_file \
   -test_file $test_file \
   -metrics $metrics \
